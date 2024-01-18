9cf2581a8662cc61ac6670b61ae47019071b09c6
Bill Doyle <vil@vil.lv>
2024-01-18 18:11:39 -0500

    Get demo loading most of the way in 4.2
    Lots of broken code, but the extension loads.

--- demo/Main.gd
+++ demo/Main.gd
@@ -6,7 +6,7 @@ func _process(delta):
 		get_tree().quit()
 	elif (Input.is_key_pressed(KEY_SPACE)):
 		# Calling center_on_hmd will cause the ARVRServer to adjust all tracking data so the player is centered on the origin point looking forward
-		XRServer.center_on_hmd(true, true)
+		XRServer.center_on_hmd(XRServer.RESET_FULL_ROTATION, true)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
