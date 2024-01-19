fcc0b181a259e64f466f8aef572ea9ba3e512a8f
Bill Doyle <vil@vil.lv>
2024-01-18 20:16:56 -0500

    Fix getter names in ButtonStates
    I kind of guessed at these, I couldn't figure out where the old names
    were defined.

--- demo/player/ButtonStates.gd
+++ demo/player/ButtonStates.gd
@@ -12,19 +12,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if controller:
-		var primary = controller.get_axis("primary")
+		var primary = controller.get_vector2("primary")
 		$VBoxContainer/PrimaryJoyX/Value.value = 100.0 * primary.x
 		$VBoxContainer/PrimaryJoyY/Value.value = 100.0 * primary.y
 		
-		var secondary = controller.get_axis("secondary")
+		var secondary = controller.get_vector2("secondary")
 		$VBoxContainer/SecondaryJoyX/Value.value = 100.0 * secondary.x
 		$VBoxContainer/SecondaryJoyY/Value.value = 100.0 * secondary.y
 
-		var trigger = controller.get_value("trigger_value")
+		var trigger = controller.get_float("trigger_value")
 		$VBoxContainer/Trigger/Value.value = 100.0 * trigger
 		$VBoxContainer/Trigger/Pressed.button_pressed = controller.is_button_pressed("trigger_click")
 
-		var grip = controller.get_value("grip_value")
+		var grip = controller.get_float("grip_value")
 		$VBoxContainer/Grip/Value.value = 100.0 * grip
 		$VBoxContainer/Grip/Pressed.button_pressed = controller.is_button_pressed("grip_click")
 		$VBoxContainer/AX/Pressed.button_pressed = controller.is_button_pressed("ax")
